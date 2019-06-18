d193c7505cbd91483d398390de2b3b93d1cfc5a1
Lu Qiu <luqiujob@gmail.com>
2019-06-18 15:11:09 -0700

    change to async

--- core/common/src/main/java/alluxio/conf/PropertyKey.java
+++ core/common/src/main/java/alluxio/conf/PropertyKey.java
@@ -2823,7 +2823,7 @@ public final class PropertyKey implements Comparable<PropertyKey> {
           .build();
   public static final PropertyKey USER_FILE_WRITE_TYPE_DEFAULT =
       new Builder(Name.USER_FILE_WRITE_TYPE_DEFAULT)
-          .setDefaultValue("MUST_CACHE")
+          .setDefaultValue("ASYNC_THROUGH")
           .setDescription("Default write type when creating Alluxio files. Valid options are "
               + "`MUST_CACHE` (write will only go to Alluxio and must be stored in Alluxio), "
               + "`CACHE_THROUGH` (try to cache, write to UnderFS synchronously), `THROUGH` "
