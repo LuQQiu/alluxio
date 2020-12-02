cc7fc941f05664103b8b58e43f24c832a5e8d0b1
Bin Fan <fanbin103@gmail.com>
2020-12-02 11:49:41 -0800

    Restore lock striping on workers

--- core/server/worker/src/test/java/alluxio/worker/block/BlockLockManagerTest.java
+++ core/server/worker/src/test/java/alluxio/worker/block/BlockLockManagerTest.java
@@ -15,9 +15,9 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
-import alluxio.conf.ServerConfiguration;
-import alluxio.conf.PropertyKey;
 import alluxio.collections.ConcurrentHashSet;
+import alluxio.conf.PropertyKey;
+import alluxio.conf.ServerConfiguration;
 import alluxio.exception.BlockDoesNotExistException;
 import alluxio.exception.ExceptionMessage;
 import alluxio.exception.InvalidWorkerStateException;
@@ -37,8 +37,14 @@ import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Unit tests for {@link BlockLockManager}.
@@ -275,8 +281,11 @@ public final class BlockLockManagerTest {
     final int numBlocks = 2;
     final int threadsPerBlock = 100;
     final int lockUnlocksPerThread = 50;
+    Map<Long, Set<Long>> sessionIdToLockIdsMap = new HashMap<>();
+    Map<Long, BlockLockManager.LockRecord> lockIdToRecordMap = new HashMap<>();
     setMaxLocks(numBlocks);
-    final BlockLockManager manager = new BlockLockManager();
+    final BlockLockManager manager = new BlockLockManager(
+        sessionIdToLockIdsMap ,lockIdToRecordMap);
     final List<Thread> threads = new ArrayList<>();
     final CyclicBarrier barrier = new CyclicBarrier(numBlocks * threadsPerBlock);
     // If there are exceptions, we will store them here.
@@ -324,7 +333,34 @@ public final class BlockLockManagerTest {
       }
       Assert.fail(sb.toString());
     }
-    manager.validate();
+    validateState(sessionIdToLockIdsMap, lockIdToRecordMap);
+  }
+
+  /**
+   * Checks the internal state of the manager to make sure invariants hold.
+   * A runtime exception will be thrown if invalid state is encountered.
+   */
+  public void validateState(Map<Long, Set<Long>> sessionIdToLockIdsMap,
+    Map<Long, BlockLockManager.LockRecord> lockIdToRecordMap) {
+    // Compute block lock reference counts based off of lock records
+    ConcurrentMap<Long, AtomicInteger> blockLockReferenceCounts = new ConcurrentHashMap<>();
+    for (BlockLockManager.LockRecord record : lockIdToRecordMap.values()) {
+      blockLockReferenceCounts.putIfAbsent(record.getBlockId(), new AtomicInteger(0));
+      blockLockReferenceCounts.get(record.getBlockId()).incrementAndGet();
+    }
+
+    // Check that if a lock id is mapped to by a session id, the lock record for that lock id
+    // contains that session id.
+    for (Map.Entry<Long, Set<Long>> entry : sessionIdToLockIdsMap.entrySet()) {
+      for (Long lockId : entry.getValue()) {
+        BlockLockManager.LockRecord record = lockIdToRecordMap.get(lockId);
+        if (record.getSessionId() != entry.getKey()) {
+          throw new IllegalStateException("The session id map contains lock id " + lockId
+              + "under session id " + entry.getKey() + ", but the record for that lock id ("
+              + record + ")" + " doesn't contain that session id");
+        }
+      }
+    }
   }
 
   private void setMaxLocks(int maxLocks) {
