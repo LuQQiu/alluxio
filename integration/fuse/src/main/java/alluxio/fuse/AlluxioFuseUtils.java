99dac2dbe624260e783140dfa378c4c880c49924
Lu Qiu <luqiujob@gmail.com>
2022-06-23 16:41:13 -0700

    Remove rm recursive

--- integration/fuse/src/main/java/alluxio/fuse/AlluxioFuseUtils.java
+++ integration/fuse/src/main/java/alluxio/fuse/AlluxioFuseUtils.java
@@ -112,7 +112,7 @@ public final class AlluxioFuseUtils {
    */
   public static void deletePath(FileSystem fileSystem, AlluxioURI uri) {
     try {
-      fileSystem.delete(uri, DeletePOptions.newBuilder().setRecursive(true).build());
+      fileSystem.delete(uri);
     } catch (IOException | AlluxioException e) {
       throw new RuntimeException(e);
     }
