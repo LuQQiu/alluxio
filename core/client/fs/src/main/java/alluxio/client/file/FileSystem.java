476917512d29b1f0bb335b11a909cf5263f7b771
Lu Qiu <luqiujob@gmail.com>
2022-10-19 11:33:22 -0700

    Add filesystem create ufs base

--- core/client/fs/src/main/java/alluxio/client/file/FileSystem.java
+++ core/client/fs/src/main/java/alluxio/client/file/FileSystem.java
@@ -16,6 +16,7 @@ import alluxio.ClientContext;
 import alluxio.annotation.PublicApi;
 import alluxio.client.file.cache.CacheManager;
 import alluxio.client.file.cache.LocalCacheFileSystem;
+import alluxio.client.file.ufs.UfsBaseFileSystem;
 import alluxio.conf.AlluxioConfiguration;
 import alluxio.conf.Configuration;
 import alluxio.conf.PropertyKey;
@@ -155,7 +156,8 @@ public interface FileSystem extends Closeable {
       if (CommonUtils.PROCESS_TYPE.get() != CommonUtils.ProcessType.CLIENT) {
         return new BaseFileSystem(context);
       }
-      FileSystem fs = new BaseFileSystem(context);
+      FileSystem fs = conf.getBoolean(PropertyKey.USER_UFS_ENABLED)
+          ? new UfsBaseFileSystem(context) : new BaseFileSystem(context);
       if (conf.getBoolean(PropertyKey.USER_METADATA_CACHE_ENABLED)) {
         fs = new MetadataCachingFileSystem(fs, context);
       }
